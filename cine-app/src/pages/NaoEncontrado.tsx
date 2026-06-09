59861e7aba6751464b72f369b7d70562b537c1a0
Guilherme <guilhermesouzaadam@gmail.com>
2026-06-08 22:20:46 -0300

    ativ 11

---
+++ cine-app/src/pages/NaoEncontrado.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+
+function NaoEncontrado() {
+  return (
+    <div>
+      <h1>404</h1>
+
+      <p>Página não encontrada.</p>
+
+      <Link to="/">
+        Voltar para a página inicial
+      </Link>
+    </div>
+  );
+}
+
+export default NaoEncontrado;
---
+++ cine-app/src/pages/Sobre.tsx
@@ -0,0 +1,14 @@
+function Sobre() {
+  return (
+    <div>
+      <h1>Sobre</h1>
+
+      <p>
+        Projeto desenvolvido para praticar
+        React, TypeScript e React Router.
+      </p>
+    </div>
+  );
+}
+
+export default Sobre;
